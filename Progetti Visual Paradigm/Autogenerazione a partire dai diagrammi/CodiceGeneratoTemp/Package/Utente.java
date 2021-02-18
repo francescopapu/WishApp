a6024d3e8973ccff94719e58ec103ac6a08b567a
frapietra <67550607+frapietra@users.noreply.github.com>
2021-02-18 22:53:11 +0100

    .
    Aggiornamento codice e powerpoint.
    Ordine.

---
+++ Progetti Visual Paradigm/Autogenerazione a partire dai diagrammi/CodiceGeneratoTemp/Package/Utente.java
@@ -0,0 +1,13 @@
+package ClassDiagram.Package;
+
+import java.util.*;
+
+public class Utente {
+
+	ArrayList<Partecipazione> partecipazione;
+	ArrayList<ListaDesideri> ListeDesideri;
+	private int ID;
+	private String Nome;
+	private String Cognome;
+
+}
